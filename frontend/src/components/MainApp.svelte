c349ae7bd5f37caf412dfb20127f20ea2e51b3ba
David Teather <34144122+davidteather@users.noreply.github.com>
2023-06-18 15:00:28 -0500

    Component skeleton commiting so I can get auto reload by not running website on the pi

---
+++ frontend/src/components/MainApp.svelte
@@ -0,0 +1,23 @@
+<script>
+    import { onMount } from 'svelte';
+    import TakePhoto from './overlays/TakePhoto.svelte';
+    import Countdown from './overlays/Countdown.svelte';
+    import UserInfo from './overlays/UserInfo.svelte';
+    import ThankYou from './overlays/ThankYou.svelte';
+    
+    let currentStep = 0;
+    let steps = [TakePhoto, Countdown, UserInfo, ThankYou];
+
+    onMount(() => {
+        // Add your transition logic here, for example:
+        const intervalId = setInterval(() => {
+            if (currentStep < steps.length - 1) {
+                currentStep++;
+            } else {
+                clearInterval(intervalId);
+            }
+        }, 3000); // This will switch to the next component every 3 seconds
+    });
+</script>
+
+<svelte:component this={steps[currentStep]} />
